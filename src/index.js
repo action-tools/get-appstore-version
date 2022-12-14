9a4ae56611b7ffa05a687cf01f0afdb3ae480b3d
Ilya Lehchylin <i.lehchylin.dev@gmail.com>
2022-12-14 18:10:32 +0300

    [UPDATE] iTunes lookup & json output

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
 import core from '@actions/core'
-import request from './request.js'
+import util from 'node:util'
+import { appstoreConnectApiRequest, itunesLookupRequest } from './request.js'
 import Utils from './utils.js'
+import { messages } from './messages.js'
 
 async function run() {
+  const isItunesLookup = core.getInput('is-itunes-lookup')
+  isItunesLookup ? await itunesLookup() : await appstoreConnectApi()
+}
+
+async function itunesLookup() {
+  try {
+    const bundleId = core.getInput('bundle-id')
+    const useHttps = core.getInput('use-https')
+
+    if (!bundleId) {
+      console.log(messages.bundle_id_not_defined)
+
+      if (!await tryAppStoreConnectApi()) {
+        throw new Error(messages.itunes_lookup_bundle_id_error)
+      }
+    }
+
+    console.log(messages.sending_itunes_lookup_request)
+    const jsonObject = await itunesLookupRequest(bundleId, useHttps)
+    const resultFailure = jsonObject.resultCount === undefined || jsonObject.resultCount < 1
+
+    if (resultFailure && !await tryAppStoreConnectApi()) {
+      throw new Error(messages.itunes_lookup_general_error)
+    }
+
+    console.log(messages.itunes_lookup_request_success)
+    const jsonOutput = JSON.stringify(jsonObject)
+    const result = jsonObject.results[0]
+    const version = result.version
+    const createdDate = result.currentVersionReleaseDate
+    core.setOutput(`app-version-latest`, version)
+    core.setOutput(`version-created-date-latest`, createdDate)
+    core.setOutput(`versions-output-json`, jsonOutput)
+    console.log(messages.action_success)
+  } catch (error) {
+    console.log(error)
+    console.log(messages.action_failed_error)
+    core.setFailed(error)
+  }
+}
+
+async function appstoreConnectApi() {
   try {
     const appId = core.getInput('app-id')
     const keyId = core.getInput('key-id')
@@ -15,52 +59,75 @@ async function run() {
 
     const utils = new Utils()
     const tokenString = utils.getToken(
-      appId, 
-      issuerId, 
-      keyId, 
-      jsonWebToken, 
-      privateKeyRaw, 
-      privateKeyFilePath, 
+      appId,
+      issuerId,
+      keyId,
+      jsonWebToken,
+      privateKeyRaw,
+      privateKeyFilePath,
       privateKeyFileBase64)
 
     const limit = utils.getLimit(versionsLimit)
+    console.log(messages.sending_appstore_connect_request)
+    const jsonObject = await appstoreConnectApiRequest(appId, tokenString, limit)
 
-    console.log("Sending App Store Connect API request...")
-    const jsonObject = await request(appId, tokenString, limit)
+    if (jsonObject.errors !== undefined && jsonObject.errors.length > 0) {
+      const error = jsonObject.errors[0];
+      throw new Error(`${messages.request_failed_with_message}:
+      id: ${error.id}
+      status: ${error.status}
+      title: ${error.title}
+      code: ${error.code}
+      detail: ${error.detail}`)
+    }
+
+    const jsonOutput = JSON.stringify(jsonObject)
     const data = jsonObject.data
 
     if (data === undefined || data.length === 0) {
-      throw new Error('Invalid request. Please check your inputs.')
+      throw new Error(messages.invalid_request)
     }
 
-    console.log("App Store Connect API request was successful, proceeding...")
-
-    const json = JSON.stringify(jsonObject)
+    console.log(messages.appstore_connect_api_request_success)
 
     if (data.length > 0) {
-      console.log("Setting outputs for the latest app version...")
-      setOutput(data, 0, json)
+      console.log(messages.setting_outputs_latest)
+      setOutput(data, 0)
     }
 
-    if (data.length === 2) {
-      console.log("Setting outputs for the previous app version...")
-      setOutput(data, 1, json)
+    if (data.length > 1) {
+      console.log(messages.setting_outputs_previous)
+      setOutput(data, 1)
     }
 
-    console.log("The action finished successfully.")
+    core.setOutput(`versions-output-json`, jsonOutput)
+    console.log(messages.action_success)
   } catch (error) {
     console.log(error)
-    console.log("The action finished with error.")
+    console.log(messages.action_failed_error)
     core.setFailed(error)
   }
 }
 
-function setOutput(data, index, jsonOutput) {
+async function tryAppStoreConnectApi() {
+  const tryApiOnFailure = core.getInput('itunes-lookup-try-api-on-failure')
+
+  if (tryApiOnFailure === true) {
+    console.log(messages.trying_appstore_connect_api_on_failure)
+    await appstoreConnectApi()
+    return true
+  }
+
+  return false
+}
+
+function setOutput(data, index) {
   const type = index === 0 ? 'latest' : 'previous'
   const attributes = data[index].attributes
 
   if (attributes == null) {
-    throw new Error(`Something went wrong. Couldn't retrieve details for the ${type} app release...`)
+    const message = util.format(messages.set_output_error, 'latest')
+    throw new Error(message)
   }
 
   const version = attributes.versionString
@@ -76,7 +143,6 @@ function setOutput(data, index, jsonOutput) {
   core.setOutput(`app-state-${type}`, state)
   core.setOutput(`app-release-type-${type}`, releaseType)
   core.setOutput(`version-created-date-${type}`, createdDate)
-  core.setOutput(`versions-output-json`, jsonOutput)
 }
 
 run()
---
+++ src/messages.js
@@ -0,0 +1,17 @@
+export const messages = Object.freeze({
+  bundle_id_not_defined: "Bundle id is not defined for iTunes lookup.",
+  sending_itunes_lookup_request: "Sending iTunes lookup request...",
+  itunes_lookup_request_success: "iTunes lookup request was successful, proceeding...",
+  trying_appstore_connect_api_on_failure: "Trying to get application version via AppStore Connect API...",
+  itunes_lookup_bundle_id_error: "In order to use iTunes lookup you need to provide correct bundle id or you can use 'itunes-lookup-try-api-on-failure' parameter to call AppStore Connect API on iTunes lookup failures",
+  itunes_lookup_general_error: "Couldn't find any information on your request. You can use 'itunes-lookup-try-api-on-failure' parameter to call AppStore Connect API on iTunes lookup failures",
+  action_failed_error: "The action finished with error.",
+  sending_appstore_connect_request: "Sending App Store Connect API request...",
+  request_failed_with_message: "Request failed with message",
+  invalid_request: "Invalid request. Please check your inputs.",
+  appstore_connect_api_request_success: "App Store Connect API request was successful, proceeding...",
+  setting_outputs_latest: "Setting outputs for the latest app version...",
+  setting_outputs_previous: "Setting outputs for the previous app version...",
+  action_success: "The action finished successfully.",
+  set_output_error: "Something went wrong. Couldn't retrieve details for the %s app release..."
+})
